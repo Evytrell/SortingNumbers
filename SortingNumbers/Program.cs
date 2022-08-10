f6b01ebac2ecb3e8bc07218be3c48d56d11ec7f1
Evelyn Nwokedi <evytrell3@gmail.com>
2022-08-10 09:00:07 +0100

    Add project files.

---
+++ SortingNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SortingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int[] newArray = new int[] { 34, 67, 9, 22, 43, 56, 9, 47 };
+                int example;
+
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    for (int arrange = i + 1; arrange < newArray.Length; arrange++)
+                    {
+                        if (newArray[i] < newArray[arrange])
+                        {
+                            example = newArray[i];
+                            newArray[i] = newArray[arrange];
+                            newArray[arrange] = example;
+                        } 
+                    }
+                }
+                foreach (int values in newArray)
+                {
+                    Console.WriteLine(values + " ");
+                }
+        }
+    }
+}
